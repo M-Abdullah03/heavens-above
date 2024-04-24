1b89b6b58f15ab9c1f90774232b9c0189932ce69
M-Abdullah03 <m18ab03@gmail.com>
2024-04-24 21:05:47 +0500

    Part 2 and 3

---
+++ generate_logs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the directory to store the log files
+log_dir="/logs"
+
+# Get the current date and time
+now=$(date +"%m_%d_%Y_%H_%M_%S")
+
+# Create a new log file with the current date and time in its name
+log_file="$log_dir/log_$now.log"
+
+# Write a message to the log file
+echo "This is a log file created on $now." > "$log_file"
